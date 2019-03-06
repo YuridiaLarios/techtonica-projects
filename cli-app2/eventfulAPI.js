486efc6bf7c79254be51843dd1eb3732aca6a07d
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-03-05 20:45:47 -0800

    commenting eventfulApi

--- cli-app2/eventfulAPI.js
+++ cli-app2/eventfulAPI.js
@@ -1,19 +1,26 @@
 const eventfulKey = require("./keys.js").eventful;
-const eventful = require('eventful-node');
+const eventful = require('eventful-node'); // documentation for functions: https://github.com/sedouard/eventful-node
 const client = new eventful.Client(eventfulKey);
 
 //sample search, try running it to see it in action
 client.searchEvents({
+  // options for search
   keywords: 'tango',
   location: 'San Francisco',
   date: "Next Week"
-}, function (err, data) {
+  // callback:
+}, function (err, data) { // error handling
   if (err) {
     return console.error(err);
   }
+
+  // saves array of resuls
   let resultEvents = data.search.events.event;
+
   console.log('Received ' + data.search.total_items + ' events');
   console.log('Event listings: ');
+
+  // using loop to console all events found and their info 
   for (let i = 0; i < resultEvents.length; i++) {
     console.log("===========================================================")
     console.log('title: ', resultEvents[i].title);
